331b85cd87d0f969546fb0ed92d3228917261a9d
VSchaepertoens <vschaepertoens@protonmail.com>
2024-02-08 17:06:56 +0100

    minor changes

--- subunits_plot_heatmap.R
+++ subunits_plot_heatmap.R
@@ -6,10 +6,12 @@ library(viridis)
 
 # load_data ---------------------------------------------------------------
 
-data <- read_csv('data/RelQuantIntact.csv') %>%
+data <- read_csv('data/RelQuantIntact01.csv') %>%
   column_to_rownames(var = "...1")
 
-# plot heatmap of raw data ------------------------------------------------
+
+# heatmap -----------------------------------------------------------------
+## plot heatmap of raw data ------------------------------------------------
 
 data.matrix <- as.matrix(data)
 
@@ -17,7 +19,7 @@ Heatmap(data.matrix)
 Heatmap(data.matrix, col = plasma(100))
 Heatmap(data.matrix, col = rev(rainbow(10)))
 
-# calculate z-score & plot heatmap -------------------------------------------------------
+## calculate z-score & plot heatmap -------------------------------------------------------
 
 scaled.data.matrix = t(scale(t(data.matrix))) # for scaling by row  
 
@@ -53,7 +55,7 @@ data_averaged <- data %>%
   group_by(cell_variant) %>%
   mutate(percent = (mean_peak_area/sum(mean_peak_area)))
 
-#convert 'subunit' to factor and specify level order
+#convert 'subunit' to factor and specify level order ## important for plotting order
 data_averaged$subunit <- factor(data_averaged$subunit, levels = c('Intact', 'LC/LC', 'LC'))
 
 # plot stacked bar chart -------------------------------------------------
@@ -63,6 +65,8 @@ ggplot(data_averaged, aes(y = cell_variant, x = mean_peak_area, fill = subunit))
   xlab("peak_area (%)") +
   scale_fill_brewer(palette = "Accent") +
   scale_y_discrete(limits = rev)
+
+ggsave("figures/stacked_bar/stacked_bar_all.png", dpi = 600)
 
 #plot stacked barchart with labels
 ggplot(data_averaged, aes(x = cell_variant, y = mean_peak_area, fill = subunit)) + 
@@ -86,7 +90,7 @@ ggplot(data_averaged, aes(x = cell_variant, y = mean_peak_area, fill = subunit))
     axis.text.x = element_blank()
     ) 
  
-ggsave("figures/stacked_bar/stacked_bar_percent.png")
+ggsave("figures/stacked_bar/stacked_bar_percent_all.png", dpi = 600)
 
 
 
