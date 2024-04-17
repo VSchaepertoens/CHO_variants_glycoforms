f4854f55118a760ef75f03fa6712e0c1b4c24ff0
VSchaepertoens <vschaepertoens@protonmail.com>
2024-04-17 13:00:09 +0200

    reorders ylabels

--- plot_sample.R
+++ plot_sample.R
@@ -5,9 +5,9 @@ library(circlize)
 
 # load an overview table of data & analysis paths -------------------------
 
-# samples_table <- read_csv("data/Jan_2024/overview_sample_merged.csv")
+samples_table <- read_csv("data/Jan_2024/overview_sample_merged.csv")
 # samples_table <- read_csv("data/Dec_2023/overview_sample_pngase_merged.csv")
-samples_table <- read_csv("data/Jan_2024/overview_sample_pngase_merged.csv")
+# samples_table <- read_csv("data/Jan_2024/overview_sample_pngase_merged.csv")
 
 
 # load abundances using a for loop  ---------------------------------------
@@ -85,8 +85,9 @@ abundance_data_averaged <- abundance_data %>%
   group_by(modcom_name, CHO_cell_variant_bio_replicate) %>%
   summarise(frac_abundance = mean(frac_ab),
             error = sd(frac_ab)) %>%
+  mutate(modcom_name = factor(modcom_name, levels = c("G1F/S1G1F","G2F/G2F","G1F/G2F","G1F/G1F","G0F/G1F","G0F/G0F","G0F/G0", "none/G2F", "none/G1F", "none/G0F", "none/G0","none/none")))
   # mutate(modcom_name = factor(modcom_name, levels = c("2xHex","1xHex","Lys/Lys","none/Lys","none/none")),
-  mutate(modcom_name = factor(modcom_name, levels = c("2xHex","1xHex","none/none"))) %>%
+  # mutate(modcom_name = factor(modcom_name, levels = c("2xHex","1xHex","none/none"))) %>%
   # filter(CHO_cell_variant_bio_replicate == c("A16_1","A16_2","A19_1","A19_2"))
 {.}
   
@@ -118,9 +119,9 @@ abundance_data_averaged %>%
   ) +
   scale_fill_brewer(palette = "Paired") +
   xlab("") +
-  ylim(0, 100) +
+  ylim(0, 60) +
   ylab("fractional abundance (%)") +
-  labs(title = "Glucose variants - intact") +
+  labs(title = "Glycoforms - intact") +
   geom_hline(yintercept = 0, linewidth = .35) +
   coord_flip() +
   theme_bw() +
@@ -137,7 +138,7 @@ abundance_data_averaged %>%
 )
 
 
-ggsave(filename = "figures/Jan_2024/frac_ab_barplot_intact_glucose.png",    
+ggsave(filename = "figures/Jan_2024/frac_ab_barplot_intact_reordered.png",    
        height = 160,
        width = 160,
        units = "mm",
