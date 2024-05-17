31b08964f9d19dee044676d4b5717a9c59850e5c
VSchaepertoens <vschaepertoens@protonmail.com>
2024-05-17 12:47:02 +0200

    changes order of variants plotted

--- subunits_plot_heatmap.R
+++ subunits_plot_heatmap.R
@@ -53,30 +53,43 @@ data_averaged <- data %>%
   group_by(cell_variant,subunit) %>%
   summarise(mean_peak_area = mean(peak_area)) %>%
   group_by(cell_variant) %>%
-  mutate(percent = (mean_peak_area/sum(mean_peak_area)))
+  mutate(percent = (mean_peak_area/sum(mean_peak_area))) %>%
+  mutate(subunit = str_replace_all(subunit, c('LC/LC' = 'LC-LC'))) %>%
+  mutate(cell_variant = factor(cell_variant, levels = c("A25_1", "A25_2", "A2_1", "A2_2", "A3_1", "A3_2", "A4_1", "A4_2", "A8_1", "A8_2", "A16_1", "A16_2", "A19_1", "A19_2")))
+
+# mutate(glycoform1 = str_replace_all(glycoform1, c("G0F/G2F" = "G1F/G1F", "G2F/none" = "none/G2F", "G1F/none" = "none/G1F", "G0F/none" = "none/G0F", "G0/none" = "none/G0"))) %>%
+  
 
 #convert 'subunit' to factor and specify level order ## important for plotting order
-data_averaged$subunit <- factor(data_averaged$subunit, levels = c('Intact', 'LC/LC', 'LC'))
+data_averaged$subunit <- factor(data_averaged$subunit, levels = c('Intact', 'LC-LC', 'LC'))
 
 # plot stacked bar chart -------------------------------------------------
 
 ggplot(data_averaged, aes(y = cell_variant, x = mean_peak_area, fill = subunit)) + 
-  geom_bar(stat = "identity", position = "fill") +
+  geom_bar(stat = "identity", position = "fill", width = .7) +
   xlab("peak_area (%)") +
+  ylab("CHO cell variant") +
   scale_fill_brewer(palette = "Accent") +
   scale_y_discrete(limits = rev) +
-  theme(text = element_text(size = 20, 
-                            face = "bold", 
+  theme(text = element_text(size = 9, 
+                            # face = "bold", 
                             family = "sans"),
         axis.text = element_text(colour = "black"),
-        # axis.ticks.y = element_blank(),
-        # legend.title = element_blank(),
-        # panel.border = element_blank(),
-        # panel.grid.major.y = element_blank(),
-        # panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.text.y = element_text(margin = margin(r = 0)),
+        axis.ticks.y = element_blank(),
+        legend.title = element_blank(),
+        legend.position = "bottom",
+        panel.border = element_blank(),
+        panel.grid.major.y = element_blank(),
+        panel.grid.minor = element_blank(),
   )
 
-ggsave("figures/stacked_bar/stacked_bar_all_bold_20_A2newdata.png", dpi = 600)
+ggsave("figures/stacked_bar/figure_8_reordered.png",        
+       width = 8.89,
+       height = 8.89,
+       units = c("cm"),
+       dpi = 600)
 
 #plot stacked barchart with labels
 ggplot(data_averaged, aes(x = cell_variant, y = mean_peak_area, fill = subunit)) + 
@@ -100,7 +113,11 @@ ggplot(data_averaged, aes(x = cell_variant, y = mean_peak_area, fill = subunit))
     axis.text.x = element_blank()
     ) 
  
-ggsave("figures/stacked_bar/stacked_bar_percent_all.png", dpi = 600)
+ggsave("figures/stacked_bar/stacked_bar_percent_all_SC.png", 
+       width = 8.89,
+       height = 6.5,
+       units = c("cm"),
+       dpi = 600)
 
 
 
