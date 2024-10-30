88fa0a542abcc36bcb5a5c04ac5a5983055adf9f
VSchaepertoens <vschaepertoens@protonmail.com>
2024-10-30 12:46:01 +0100

    minor changes

--- plot_sample.R
+++ plot_sample.R
@@ -6,9 +6,10 @@ library(RColorBrewer)
 
 # load an overview table of data & analysis paths -------------------------
 
-# samples_table <- read_csv("data/Jan_2024/overview_sample_merged.csv")
+samples_table <- read_csv("data/2_nglycans_quantification/2_1_not_corrected_glycans/overview_sample.csv")  %>%
+  filter(CHO_cell_variant != "A25")
 # samples_table <- read_csv("data/Dec_2023/overview_sample_pngase_merged.csv")
-samples_table <- read_csv("data/Jan_2024/overview_sample_pngase_merged.csv")
+# samples_table <- read_csv("data/Jan_2024/overview_sample_pngase_merged.csv")
 
 
 # load abundances using a for loop  ---------------------------------------
@@ -86,17 +87,18 @@ abundance_data_averaged <- abundance_data %>%
   group_by(modcom_name, CHO_cell_variant_bio_replicate) %>%
   summarise(frac_abundance = mean(frac_ab),
             error = sd(frac_ab)) %>%
-  # mutate(modcom_name = factor(modcom_name, levels = c("G1F/S1G1F","G2F/G2F","G1F/G2F","G1F/G1F","G0F/G1F","G0F/G0F","G0F/G0", "none/G2F", "none/G1F", "none/G0F", "none/G0","none/none")))
+  mutate(modcom_name = factor(modcom_name, levels = c("G1F/S1G1F","G2F/G2F","G1F/G2F","G1F/G1F","G0F/G1F","G0F/G0F","G0F/G0", "none/G2F", "none/G1F", "none/G0F", "none/G0","none/none")))
   # mutate(modcom_name = str_replace_all(modcom_name, c("Lys/Lys" = "2xLys", "none/Lys" = "1xLys"))) %>%
   # mutate(modcom_name = factor(modcom_name, levels = c("2xHex","1xHex","2xLys","1xLys","none/none"))) %>%
   mutate(CHO_cell_variant_bio_replicate = factor(CHO_cell_variant_bio_replicate, levels = c("A19_2","A19_1", "A16_2","A16_1","A8_2", "A8_1","A4_2", "A4_1","A3_2", "A3_1","A2_2", "A2_1"))) %>%
-  mutate(modcom_name = factor(modcom_name, levels = c("2xHex","1xHex","none/none"))) %>%
+  # mutate(modcom_name = factor(modcom_name, levels = c("2xHex","1xHex","none/none"))) %>%
   # filter(CHO_cell_variant_bio_replicate == c("A16_1","A16_2","A19_1","A19_2"))
 {.}
   
 save(abundance_data,
      abundance_data_averaged,
-     file = "analysis/Jan_2024/abundance_data_cpb_pngase.RData")
+     file = "analysis/2_nglycans_quantification/2_1_not_corrected_glycans/intact_output_tables/abundance_data.RData")
+     # file = "analysis/Dec_2023/abundance_data_cpb_pngase.RData")
 # abundance_data_average_Orig <- abundance_data_averaged
 
 # load("analysis/Jan_2024/abundance_data.RData")
@@ -127,15 +129,15 @@ abundance_data_averaged %>%
     breaks = c("A2_1","A2_2", "A3_1","A3_2","A4_1", "A4_2","A8_1", "A8_2","A16_1", "A16_2","A19_1", "A19_2")
   ) +
   xlab("") +
-  ylim(0, 100) +
+  ylim(0, 40) +
   ylab("fractional abundance (%)") +
-  # labs(title = "Glycoforms - intact") +
+  labs(title = "Glycoforms - intact") +
   geom_hline(yintercept = 0, linewidth = .35) +
   coord_flip() +
   theme_bw() +
   guides(fill = guide_legend(ncol = 3)) + 
   theme(text = element_text(size = 9, 
-                            # face = "bold", 
+                            face = "bold",
                             family = "sans"),
         axis.text.y = element_text(colour = "black", hjust = 0.5),
         axis.text = element_text(colour = "black"),
@@ -153,7 +155,7 @@ abundance_data_averaged %>%
 
 
 
-ggsave(filename = "figures/Dec_2023/frac_ab_barplot_intact_SC_legend_reordered_2.png",    
+ggsave(filename = "figures/2_nglycans_quantification/2_1_not_corrected_glycans/2_1_not_corrected_glycans.png",    
        height = 8.89,
        width = 8.89,
        units = "cm",
